fe39fdf24a146e1ba9db31229e47c0df7b3f6bfb
thiagomsantos24 <thiago.martins@sinodalprado.com.br>
2025-06-11 10:07:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.expand-button').forEach(button => {
+    button.addEventListener('click', () => {
+        // Aqui você pode implementar a lógica para abrir um modal com a imagem em alta resolução e descrição detalhada
+        alert('Abrir modal com mais informações sobre a espécie.');
+    });
+});
